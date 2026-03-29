d6d4ffa6f5b18cca78a434c0acea4ebde280fbb1
rickybommaji <rickybommaji@gmail.com>
2026-03-29 22:28:05 +0530

    resolved bugs

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,6 +1,8 @@
-import { SplitText } from "gsap-trial/SplitText";
+import SplitText from "gsap-trial/SplitText";
 import gsap from "gsap";
 import { smoother } from "../Navbar";
+
+gsap.registerPlugin(SplitText); // ✅ ADD THIS LINE
 
 export function initialFX() {
   document.body.style.overflowY = "auto";
@@ -80,7 +82,7 @@ export function initialFX() {
   LoopText(landingText4, landingText5);
 }
 
-function LoopText(Text1: SplitText, Text2: SplitText) {
+function LoopText(Text1: any, Text2: any) {
   var tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
   const delay = 4;
   const delay2 = delay * 2 + 1;
